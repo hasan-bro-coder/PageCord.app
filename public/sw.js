0e9806d3baa8c0c09d88b8a232ddcc607d019039
hasan-bro-coder <hasanur23910@gmail.com>
2023-11-10 12:54:59 +0600

    22403

--- public/sw.js
+++ public/sw.js
@@ -11,35 +11,35 @@ const fontFallback = false;
  
 setDefaultHandler(new NetworkOnly());
 
-self.addEventListener('push', (event) => {
-  event.waitUntil(
-    self.registration.showNotification('new notification for no reason', {
-      body: 'idk why am i sending notification',
-      icon: './android/android-launchericon-144-144.png',
-    });
-    );
-});
+// self.addEventListener('push', (event) => {
+//   event.waitUntil(
+//     self.registration.showNotification('new notification for no reason', {
+//       body: 'idk why am i sending notification',
+//       icon: './android/android-launchericon-144-144.png',
+//     });
+//     );
+// });
 // self.addEventListener('notificationclick', (event) => {
 //   event.notification.close(); 
 //   var fullPath = self.location.origin + event.notification.data.path; 
 //   clients.openWindow(fullPath); 
 // });
 
-// self.addEventListener('install', event => {
-//   const files = [pageFallback];
-//   if (imageFallback) {
-//     files.push(imageFallback);
-//   }
-//   if (fontFallback) {
-//     files.push(fontFallback);
-//   }
+self.addEventListener('install', event => {
+  const files = [pageFallback];
+  if (imageFallback) {
+    files.push(imageFallback);
+  }
+  if (fontFallback) {
+    files.push(fontFallback);
+  }
 
-//   event.waitUntil(
-//     self.caches
-//       .open('workbox-offline-fallbacks')
-//       .then(cache => cache.addAll(files))
-//   );
-// });
+  event.waitUntil(
+    self.caches
+      .open('workbox-offline-fallbacks')
+      .then(cache => cache.addAll(files))
+  );
+});
 
 const handler = async options => {
   const dest = options.request.destination;

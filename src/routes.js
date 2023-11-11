c8193c1c3cb0b42498f162fe29f7a95b5468f721
hasan-bro-coder <hasanur23910@gmail.com>
2023-11-11 16:56:28 +0600

    v24

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router"
+import Home from './components/Home.vue'
 import Chat from './components/Chat.vue'
-// import NotFound from './components/Not.vue'
-// import Setting from './components/other/setting.vue'
-// import Login from './components/login.vue'
-// import Sign from './components/signup.vue'
-// import Inbox from './components/inbox.vue'
-import Home from './components/home.vue'
+import NotFound from './components/Not.vue'
+import Setting from './components/other/setting.vue'
+import Login from './components/login.vue'
+import Sign from './components/signup.vue'
+import Inbox from './components/inbox.vue'
+
 // import 
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		{ path: '/', component: Home},
-		// { path: "/:id", component: import('./components/Chat.vue') },
-		// { path: '/', component: import('./components/Home.vue')},
-		{ path: "/login", component: import('./components/login.vue') },
-		{ path: "/inbox", component: import('./components/inbox.vue') },
+		{ path: '/', component: Home },
+		{ path: "/login", component: Login },
+		{ path: "/inbox", component: Inbox },
 		{ path: "/:id", component: Chat },
-		{ path: "/account", component: import('./components/signup.vue') },
-		{ path: "/settings", component: import('./components/other/setting.vue') },
-		{ path: "/:pathMatch(.*)*", component:import('./components/Not.vue')}
+		{ path: "/account", component: Sign },
+		{ path: "/settings", component: Setting },
+		{ path: "/:pathMatch(.*)*", component: NotFound }
 	],
 })
 export default router;

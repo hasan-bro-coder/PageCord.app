e1c94803d44cd6f8bb77c5cd79e2f9b592efd740
hasan-bro-coder <hasanur23910@gmail.com>
2023-11-14 23:37:41 +0600

    14500

--- push.sh
+++ push.sh
@@ -1,5 +1,12 @@
 vercel && vercel --prod 
 git add .
-git commit -m "$RANDOM"
+
+if [ $1 ]; then
+    echo "\n\n commit $1 \n\n"
+    git commit -m "$1"
+  else
+    echo "\n\n commit is random \n\n"
+    git commit -m "$RANDOM"
+fi
 git push -u origin master
 npm run build

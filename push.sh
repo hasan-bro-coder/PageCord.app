1932751e57047a0815f526a72acaea7df5f09583
hasan-bro-coder <hasanur23910@gmail.com>
2023-11-14 23:44:26 +0600

    14070

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 vercel && vercel --prod 
 git add .
 
-if [ $1 ]; then
-    echo "\n\n commit $1 \n\n"
+if [ $1 != '' ]; then
+    echo -e "\n\n commit $1 \n\n"
     git commit -m "$1"
   else
-    echo "\n\n commit is random \n\n"
+    echo -e "\n\n commit is random \n\n"
     git commit -m "$RANDOM"
 fi
 git push -u origin master
